b8dfa903a7e85dca83797d5709447fc7a1e14599
YuHover <xiangyu-l@outlook.com>
2021-09-21 17:20:34 +0800

    customized responseWriter for saving response status

--- responseWriter.go
+++ responseWriter.go
@@ -9,11 +9,17 @@ type responseWriter struct {
 }
 
 func (w *responseWriter) WriteHeader(statusCode int) {
-	w.StatusCode = statusCode
+	if !w.written {
+		w.StatusCode = statusCode
+		w.written = true
+	}
 	w.ResponseWriter.WriteHeader(statusCode)
 }
 
 
 func (w *responseWriter) Write(data []byte) (int, error) {
+	if !w.written {
+		w.WriteHeader(http.StatusOK)
+	}
 	return w.ResponseWriter.Write(data)
 }
